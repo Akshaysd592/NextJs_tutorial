ccabac042c1bb2512eb12ff134c72353c7b8959e
Akshay Dhobale <akshaysd592@gamil.com>
2024-02-24 22:09:44 +0530

    border and hero section completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import HeroSection from "@/components/HeroSection";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] ">
-          <h1 className="text-center font-extrabold bg">Hello ji how are you </h1>
+          <h1 className="text-center font-extrabold">Hello ji how are you </h1>
+          <HeroSection/>
     </main>
   );
 }
